1e3ef7eddea3df91b4f96bb16ffec2cf8064beb0
Sarah Maples <sarah.e.maples@gmail.com>
2019-05-31 09:36:03 -0500

    uuugh

--- client/src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default {
-  // Saves a user to the database
-  saveUser: function(userData) {
-    return axios.post("/api/user", userData);
-  },
-};
--- routes/api/users.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const usersController = require("../../controllers/usersController.js");
-
-router.route("/")
-  .post(usersController.create);
-
-module.exports = router;
